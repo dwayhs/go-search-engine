26d8d7038a4d326ff3443f601b4a2be79cfcdcef
dwayhs <danielwayhs@gmail.com>
2017-03-28 20:09:23 -0300

    chore: Documenting public methods and structs

--- core/core.go
+++ core/core.go
@@ -1,5 +1,6 @@
 package core
 
+// Document values represent a document that can be indexed and queried
 type Document struct {
 	UID        uint32
 	Attributes map[string]string
--- indexing/indexing.go
+++ indexing/indexing.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dwayhs/go-search-engine/core"
 )
 
+// IndexStore structs implement the indexing and storage of documents with given terms
 type IndexStore interface {
 	Index(terms []analysis.Term, document core.Document) error
 	Search(terms []analysis.Term) []core.Document
